04835aa087e3cff023f3c76a7a776d13ca7c20d1
apresta <presta.alberto@libero.it>
2020-06-05 09:33:48 +0200

    launch_game_modification

--- src/main/java/Brique.java
+++ src/main/java/Brique.java
@@ -1,21 +1,13 @@
 import CLI.IO_Interface;
 
-import CLI.Menu;
+
 import Logic.Launch_Game;
 
 public class Brique {
 
     public static void main(String[] args) {
-        String inter_string = IO_Interface.ChooseInterface();
-        if (inter_string.equals("GUI")) {
-            Launch_Game game = new Launch_Game();
-            game.launch();
-        } else {
-            Menu menu = new Menu();
-            menu.Launch_menu();
-
-        }
+        boolean chosen_interface = IO_Interface.ChooseInterface();
+        Launch_Game game = new Launch_Game(chosen_interface);
+        game.launch();
     }
-
-
 }
--- src/main/java/Logic/Launch_Game.java
+++ src/main/java/Logic/Launch_Game.java
@@ -1,21 +1,63 @@
 package Logic;
 
+import CLI.Display;
+import CLI.Game_cli;
+import CLI.IO_Interface;
+import CLI.Menu;
 import GUI.MainMenu;
 
 import javax.swing.*;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Launch_Game {
+    boolean play_with_gui;
 
-    public Launch_Game() {}
+    public Launch_Game(boolean gui) {
+        this.play_with_gui = gui;
+    }
 
     public void launch() {
-        SwingUtilities.invokeLater(() -> {
-            try {
-                new MainMenu();
-            } catch (IOException e) {
-                e.printStackTrace();
+        if (play_with_gui == true) {
+            SwingUtilities.invokeLater(() -> {
+                try {
+                    new MainMenu();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        } else {
+            Player player1, player2;
+            Menu menu = new Menu();
+            AtomicBoolean exit = new AtomicBoolean(false);
+            while (!exit.get()) {
+                Display.printMenu(menu);
+                int s = IO_Interface.getMenuChoice();
+                switch (s) {
+                    case 0:
+                        player1 = new Player(PieceColor.BLACK, IO_Interface.getName(PieceColor.BLACK));
+                        System.out.println();
+                        player2 = new Player(PieceColor.WHITE, IO_Interface.getName(PieceColor.WHITE));
+                        System.out.println();
+                        Game_cli gameHum = new Game_cli(player1, player2);
+                        gameHum.playGame();
+                        break;
+                    case 1:
+                        player1 = new Player(PieceColor.BLACK);
+                        System.out.println();
+                        player2 = new Player(PieceColor.WHITE, IO_Interface.getName(PieceColor.WHITE));
+                        System.out.println();
+                        Game_cli gameAI = new Game_cli(player1, player2);
+                        gameAI.playGame();
+                        break;
+                    case 2:
+                        exit.set(true);
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + s);
+                }
             }
-        });
+            System.out.println("Bye!");
+        }
     }
 }
